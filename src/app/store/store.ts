0d65cd0e5921b1b929954a857c7e8a11d208c984
Gerardo Regalado <gerardo.regalado24@gmail.com>
2024-04-03 16:25:50 -0600

    Fix(): Changing class logic in order to make it auto observable since I have a bug when removing task

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,4 +1,4 @@
-import { observable, action } from 'mobx';
+import { action, makeAutoObservable } from 'mobx';
 
 /**
  *  ?   observable:  
@@ -7,19 +7,38 @@ import { observable, action } from 'mobx';
  *          marks a method as an action that will modify the state.
  *  !   computed:
  *  !       Needs to study!
+ *  ?   makeAutoObservable:
+ *          is an easy way to convert to observables fields and methods.
  */
 
-class todoStore {
-  @observable todos: string[] = [];
 
+class todoStore {
+  public todos: string[] = [];
+
+  /**
+   * setting up the class to be an observable
+   */
+  constructor() {
+    makeAutoObservable(this)
+  }
+  
+
+  /**
+   * it pushes the task into the `todos` array modifying the state of the todo list.
+   * @param todo as string.
+   */
   @action
   addTodo(todo: string) {
     this.todos.push(todo);
   }
 
+  /**
+   * it splices the task into the `todos` array from the index passed as argument.
+   * @param index as number
+   */
   @action
   removeTodo(index: number) {
-    this.todos.splice(index, 1);
+    this.todos.splice(index, 1)
   }
 }
 
@@ -32,4 +51,6 @@ export default TodoStore;
  * ?    Annotations:
  * ?        Computed,
  * ?        observable: does it works similar to observables in rxjs (Angular)???
+ * ?    differences between makeObservable and makeAutoObservable in Mobx;
+ * ?    
  */

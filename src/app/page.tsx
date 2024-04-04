3c8e92722e3d808040c1002472e4844820562ff0
Gerardo Regalado <gerardo.regalado24@gmail.com>
2024-04-03 21:11:55 -0600

    Feat(): removed global styles and importing bootstrap styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import TodoApp from "../../pages/todo.page";
 import styles from "./page.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Home() {
   return (

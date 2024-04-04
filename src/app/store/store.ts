a3c12dfbf9ab2cdbebd4a87be7e23888900d66fc
Gerardo Regalado <gerardo.regalado24@gmail.com>
2024-04-03 18:39:13 -0600

    Fix(): Changing logic into the store in order to use the interface, also setting the done property and creating the handle for this

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,6 +1,57 @@
+
+// --> Package Imports
 import { action, makeAutoObservable } from 'mobx';
 
+class todoStore {
+  public todos: Task[] = [];
+
+  /**
+   * * setting up the class to be an observable
+   */
+  constructor() {
+    makeAutoObservable(this)
+  }
+  
+
+  /**
+   * * it pushes the task into the todos array modifying the state of the todo list.
+   * @param todo as string.
+   */
+  @action
+  addTask(todo: string): void {
+    const newTask = { // -> in order to comply with Task interface and setting up the done property as false;
+      title: todo, 
+      done: false,
+    } 
+    this.todos.push(newTask);
+  }
+
+  /**
+   * * it splices the task into the todos array from the index passed as argument.
+   * @param index as number
+   */
+  @action
+  removeTask(index: number): void {
+    this.todos.splice(index, 1)
+  }
+
+  /**
+   * * it marks a task as done or not done with each click in the checkbox located in taskCard component.
+   * @param index as number
+   */
+  @action
+  toggleDone(index: number): void {
+    this.todos[index].done = !this.todos[index].done
+  }
+}
+
+const TodoStore = new todoStore();
+export default TodoStore;
+
 /**
+ * -----------------------------------------------------------------------------------------
+ *  *   Notes:
+ * 
  *  ?   observable:  
  *         defines a trackable field that stores the state.
  *  ?   action:      
@@ -11,46 +62,12 @@ import { action, makeAutoObservable } from 'mobx';
  *          is an easy way to convert to observables fields and methods.
  */
 
-
-class todoStore {
-  public todos: string[] = [];
-
-  /**
-   * setting up the class to be an observable
-   */
-  constructor() {
-    makeAutoObservable(this)
-  }
-  
-
-  /**
-   * it pushes the task into the `todos` array modifying the state of the todo list.
-   * @param todo as string.
-   */
-  @action
-  addTodo(todo: string) {
-    this.todos.push(todo);
-  }
-
-  /**
-   * it splices the task into the `todos` array from the index passed as argument.
-   * @param index as number
-   */
-  @action
-  removeTodo(index: number) {
-    this.todos.splice(index, 1)
-  }
-}
-
-const TodoStore = new todoStore();
-export default TodoStore;
-
-
 /**
  * ! Topics for studying:
  * ?    Annotations:
  * ?        Computed,
  * ?        observable: does it works similar to observables in rxjs (Angular)???
  * ?    differences between makeObservable and makeAutoObservable in Mobx;
- * ?    
+ *     
+ * --------------------------------------------------------------------------------------------
  */

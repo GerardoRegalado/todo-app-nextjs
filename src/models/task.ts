a0b34610bf2bceb823761a0e165e02edf5b6cc78
Gerardo Regalado <gerardo.regalado24@gmail.com>
2024-04-03 18:38:19 -0600

    Feat(): Created new interface to work with strong typing

---
+++ src/models/task.ts
@@ -0,0 +1,4 @@
+interface Task {
+    title: string; // --> used as a property to save the title of the task.
+    done: boolean; // --> used as a flag to let the app know when the task is complete or not.
+}
